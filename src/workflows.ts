9f303b853193f3a98cc9da28bee89956651890d9
Loren 🤓 <lorensr@gmail.com>
2023-01-15 17:10:15 -0500

    It's fixed, I promise

--- src/workflows.ts
+++ src/workflows.ts
@@ -25,9 +25,9 @@ export async function backgroundCheck({ customerId, userId }: BackgroundCheckInp
     approvalId = await getApprovalStatus({ customerId, userId, targetStatus: 'complete' })
 
     await Promise.all([
-      async () => (ssnSearchId = await performSearch({ type: 'ssn', customerId, userId })),
-      async () => (creditSearchId = await performSearch({ type: 'credit', customerId, userId })),
-      async () => (socialSearchId = await performSearch({ type: 'social', customerId, userId })),
+      (async () => (ssnSearchId = await performSearch({ type: 'ssn', customerId, userId })))(),
+      (async () => (creditSearchId = await performSearch({ type: 'credit', customerId, userId })))(),
+      (async () => (socialSearchId = await performSearch({ type: 'social', customerId, userId })))(),
     ])
   } catch (err) {
     console.log((err as Error).message)
